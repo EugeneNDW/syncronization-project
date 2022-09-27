dee5b95fae381a3ad9081617018d86b3dc918927
kuznetsov <Eugene.NDW@yandex.ru>
2022-09-27 16:54:45 +0300

    refactor conversation processing

--- image-drive-bot/src/main/java/ndw/eugene/imagedrivebot/services/IFileService.java
+++ image-drive-bot/src/main/java/ndw/eugene/imagedrivebot/services/IFileService.java
@@ -1,9 +1,9 @@
 package ndw.eugene.imagedrivebot.services;
 
 import ndw.eugene.imagedrivebot.DriveSyncBot;
-import ndw.eugene.imagedrivebot.conversations.uploadPhoto.PhotoUploadData;
 import ndw.eugene.imagedrivebot.dto.FileInfoDto;
 import ndw.eugene.imagedrivebot.dto.FilesSynchronizationResponse;
+import org.telegram.telegrambots.meta.api.objects.Document;
 
 import java.io.File;
 import java.util.List;
@@ -11,7 +11,7 @@ import java.util.List;
 public interface IFileService {
     FilesSynchronizationResponse sendFileToDisk(long chatId, File fileToDisk, FileInfoDto fileInfo);
 
-    List<FilesSynchronizationResponse> synchronizeFiles(DriveSyncBot bot, long chatId, long userId, PhotoUploadData pd);
+    List<FilesSynchronizationResponse> synchronizeFiles(DriveSyncBot bot, long chatId, long userId, String description, List<Document> documents);
 
     void renameChatFolder(long chatId, String newFolderName);
 }
